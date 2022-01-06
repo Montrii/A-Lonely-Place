a3375a882476fec5bbc9d38cb2d873bf8f9cc008
Montri <jg68332@gmail.com>
2022-01-06 21:26:33 +0100

    0.1.8
    Renamed Images folder

--- files/main.js
+++ files/main.js
@@ -7,7 +7,7 @@ async function main()
     var mapHandler = new MapHandler();
     //console.log(await mapHandler.loadMap(1));
 
-    console.log("Version 0.1.7 BETA.");
+    console.log("Version 0.1.8 BETA.");
     await mapHandler.loadMap(1);
 }
 
