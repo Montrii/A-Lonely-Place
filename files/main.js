120edec0de63a2875040821d470682776e41a76a
Montri <jg68332@gmail.com>
2022-01-08 23:20:43 +0100

    Fixed

---
+++ files/main.js
@@ -0,0 +1,17 @@
+import { MapHandler } from "./modules/mapHandler.js";
+$(document).ready(main);
+
+
+async function main()
+{
+    var mapHandler = new MapHandler();
+    //console.log(await mapHandler.loadMap(1));
+
+    console.log("Version 0.1.8 BETA.");
+    await mapHandler.loadMap(1);
+}
+
+
+
+
+
