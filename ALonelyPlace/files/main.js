fd8f39f69247cd54a0f33249d5e3ac3cc174140c
Montri <jg68332@gmail.com>
2022-01-08 23:31:44 +0100

    0.2.9

--- ALonelyPlace/files/main.js
+++ ALonelyPlace/files/main.js
@@ -7,7 +7,7 @@ async function main()
     var mapHandler = new MapHandler();
     //console.log(await mapHandler.loadMap(1));
 
-    console.log("Version 0.2.8 BETA.");
+    console.log("Version 0.2.9 BETA.");
     await mapHandler.loadMap(1);
 }
 
