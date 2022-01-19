8c04e89c3cd069124032997cad8f9961697a716b
Montri <jg68332@gmail.com>
2022-01-19 21:33:13 +0100

    0.3.984

--- ALonelyPlace/files/modules/saveStats.php
+++ ALonelyPlace/files/modules/saveStats.php
@@ -6,6 +6,10 @@
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+        echo "Failed to connect with MySQL: ". mysqli_connect_error();  
+    }
+    else 
+    {
+        echo "SUCCESSFULLY LOGGED IN INTO DATABASE";
     }
 ?>
