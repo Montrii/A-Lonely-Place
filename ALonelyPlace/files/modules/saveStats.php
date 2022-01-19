585593c345cc7bd7ca83a9ee812c04307d10c6d2
Montri <jg68332@gmail.com>
2022-01-19 21:48:39 +0100

    0.3.994

--- ALonelyPlace/files/modules/saveStats.php
+++ ALonelyPlace/files/modules/saveStats.php
@@ -1,15 +1,6 @@
 <?php
-    $host = "localhost";  
-    $user = "u867191320_montri";  
-    $password = 'JrUtMK12.';  
-    $db_name = "u867191320_ALonelyPlace";  
-      
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        echo "Failed to connect with MySQL: ". mysqli_connect_error();  
-    }
-    else 
+    if($_GET['ajax'] == true)
     {
-        echo $_GET['userIp'];
+        echo "ajax request handled!";
     }
 ?>
