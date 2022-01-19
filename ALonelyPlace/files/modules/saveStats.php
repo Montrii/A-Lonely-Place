2e8eda7e43c2fdf3d195d164f82c93309ab53f15
Montri <jg68332@gmail.com>
2022-01-19 21:32:02 +0100

    0.3.983

--- ALonelyPlace/files/modules/saveStats.php
+++ ALonelyPlace/files/modules/saveStats.php
@@ -1,3 +1,11 @@
 <?php
-    echo "sdfhsdfhjsdgfjhsdfgsdjfsd";
+    $host = "localhost";  
+    $user = "u867191320_montri";  
+    $password = 'JrUtMK12.';  
+    $db_name = "u867191320_ALonelyPlace";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }
 ?>
