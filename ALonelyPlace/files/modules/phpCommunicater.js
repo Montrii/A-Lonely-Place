5bc4e67ed91cf71f8ba532f690b37dbb80c7f368
Montri <jg68332@gmail.com>
2022-01-19 21:09:17 +0100

    0.3.981

--- ALonelyPlace/files/modules/phpCommunicater.js
+++ ALonelyPlace/files/modules/phpCommunicater.js
@@ -6,18 +6,16 @@ export class phpCommunicater
     }
     sendToPHP()
     {
-        $.ajax({
-            method: 'get',
-            url: 'https://montriscript.com/projects/ALonelyPlace/ALonelyPlace/files/modules/saveStats.php',
-            data :
+        var objXMLHttpRequest = new XMLHttpRequest();
+        objXMLHttpRequest.onreadystatechange = function()
+        {
+            if(objXMLHttpRequest.readyState == 4 && objXMLHttpRequest.status == 200)
             {
-                'receivedItem': true,
-                'ajax': true
-            },
-            success: function(data)
-            {
-                alert("successfully sent data to php server");
+                console.log(objXMLHttpRequest.status);
+                console.log(objXMLHttpRequest.statusText);
             }
-        });
+        }
+        objXMLHttpRequest.open('GET', 'https://montriscript.com/projects/ALonelyPlace/ALonelyPlace/files/modules/saveStats.php');
+        objXMLHttpRequest.send();
     }
 }
