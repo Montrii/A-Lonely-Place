2804b71f5b221c2c259766b410d23cff89820b26
Montri <jg68332@gmail.com>
2022-01-20 14:35:26 +0100

    0.4.23

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -10,21 +10,5 @@
         echo false;
     }
     echo $_GET['ip'];
-    $getUserData = "select * from inventory";
-    $getUserData_result = $connection->query($getUserData);
-
-    if($getUserData_result->num_rows > 0)
-    {
-        // if user is already registered in database -> pull data from db and overwrite it.
-        while($row = $getUserData->fetch_assoc())
-        {
-            echo "id:". $row['user_id'] . " user_name: " $row['user_name'];
-        }
-    }
-    else 
-    {
-        // if no entry was made in database -> new account should be created
-        echo "NO USER IN DATABASE FOUND";
-    }
     $connection->close();
 ?>
