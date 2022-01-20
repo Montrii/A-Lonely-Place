dfe8a1d41140dcd665aeae10732e5247078e899b
Montri <jg68332@gmail.com>
2022-01-20 15:09:52 +0100

    0.4.56

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "locahost";
     $database = "u867191320_ALonelyPlace";
-    $user_db = "u867191320_montriii";
+    $user_db = "u867191320_montri";
     $password_db = "JrUtMK12.";
 
     $conn = mysqli_connect($servername, $user_db, $password_db, $database);
