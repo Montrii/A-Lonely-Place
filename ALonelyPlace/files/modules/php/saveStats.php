0c5f0656f2df102a6ca8c4d07e9ac54582cb4aca
Montri <jg68332@gmail.com>
2022-01-20 14:43:20 +0100

    0.4.35

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -4,8 +4,8 @@
     $user_db = "u867191320_montri";
     $password_db = "JrUtMK12.";
 
-    $connection = new mysqli($servername, $user_db, $password_db);
-    if (mysqli_connect_error()) 
+    $connection = new mysqli($servername, $user_db, $password_db, $database);
+    if ($connection -> connect_errno) 
     {
         echo false;
     }
