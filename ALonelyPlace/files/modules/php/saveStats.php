b0313138cae04c1631eeb38bce83490e250048d4
Montri <jg68332@gmail.com>
2022-01-20 15:35:14 +0100

    0.4.61

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $database = "u867191320_ALonelyPlace";
-    $user_db = "u867191320_montri";
-    $password_db = "JrUtMK12.";
+    $servername = "XX";
+    $database = "XX";
+    $user_db = "XX";
+    $password_db = "XX";
 
     $conn = mysqli_connect($servername, $user_db, $password_db, $database);
 
