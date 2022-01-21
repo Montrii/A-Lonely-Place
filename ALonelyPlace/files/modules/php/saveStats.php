a212abf64c8ea865a41c922040c8a0bc09858897
Montri <jg68332@gmail.com>
2022-01-21 18:22:15 +0100

    0.4.72

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -14,7 +14,7 @@
 
 
     // Get Sword slot
-    $sql = "SELECT * FROM items";
+    $sql = "SELECT * FROM items WHERE id = 1";
     $result = mysqli_query($conn, $sql);
     $sword = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo print_r($sword);
