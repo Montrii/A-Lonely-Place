c63e122723820695ba322c3e0b3b1c3a87a84278
Montri <jg68332@gmail.com>
2022-01-21 20:13:15 +0100

    0.5.26

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -26,19 +26,19 @@
         }
         else 
         {
-            echo "ITEM FOUND IN DATABASE!";
+            echo "ITEM FOUND IN DATABASE!\n";
             $sqlUser = "SELECT * FROM inventory WHERE user = '" . $_GET['ip'] . "'";
             $result = mysqli_query($conn, $sqlUser);
             $userInfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
             if(count($item) < 1)
             {
                 // insert new data into database
-                echo "USER DATA DOES NOT EXIST";
+                echo "USER DATA DOES NOT EXIST\n";
             }
             else
             {
                 // update inventory
-                echo "USER DATA EXISTS - OVERWRITING";
+                echo "USER DATA EXISTS - OVERWRITING\n";
             }
         }
     }
