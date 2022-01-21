87a05f3e6492a7aef6cd74de89f401703502d78f
Montri <jg68332@gmail.com>
2022-01-21 19:26:10 +0100

    0.5.14

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -20,6 +20,7 @@
     $result = mysqli_query($conn, $sql);
     $item = mysqli_fetch_all($result, MYSQLI_ASSOC);
     $matchsword = false;
+    /*
     foreach($item as $key => $value)
     {
         if($item['id'] == $ITEM_ID)
@@ -32,6 +33,7 @@
         echo "THIS ITEM ID DOES NOT EXIST WITHIN THE DATABASE"; 
     }
     echo "ITEM FOUND IN DATABASE!":
+    */
     echo print_r($item);
 
     /*
