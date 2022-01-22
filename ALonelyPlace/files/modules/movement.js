40644b289afacd2ac0053564f2faa182c8cb02b0
Montri <jg68332@gmail.com>
2022-01-22 22:22:04 +0100

    0.6.399

--- ALonelyPlace/files/modules/movement.js
+++ ALonelyPlace/files/modules/movement.js
@@ -16,6 +16,9 @@ export class Movement
     {
         var position = 0;
         player.createPlayerWalkSound();
+        php.saveStats({
+            item: 1
+        });
         php.getPlayerItems();
         $(document).keydown(function() {
             var keyDown = event.keyCode;
