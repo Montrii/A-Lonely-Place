a7854456b525bf1f1839185d6f63b727a3d68897
Montri <jg68332@gmail.com>
2022-01-23 12:21:09 +0100

    0.6.386

--- ALonelyPlace/files/modules/backPack.js
+++ ALonelyPlace/files/modules/backPack.js
@@ -35,7 +35,7 @@ export class backPack
             {
                 allItems[i].addEventListener("mouseover", function(event)
                 {
-                    $('body').append(`<div class="infoText ${getSlotFromItem(this.innerHTML)}" style="position:absolute;width:400px;height:200px;top:${event.clientY}px;left:${event.clientX}px;">
+                    $('body').append(`<div class="infoText ${getSlotFromItem(this.innerHTML)}" style="position:absolute;width:400px;height:400px;top:${event.clientY}px;left:${event.clientX}px;">
                     <p>THIS IS THE TITLE OF THE WEAPON</p><br><p>DESCRIPTION</p><br><p>EFFECT></p><br><p>RAREITY LEVEL</p></div>`);
                 });
                 allItems[i].addEventListener("mouseout", function(event)
