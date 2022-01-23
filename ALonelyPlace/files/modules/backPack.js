1bdc5a89cc7e6d50aea1537a7403ec956653fd1f
Montri <jg68332@gmail.com>
2022-01-23 11:01:38 +0100

    0.6.31

--- ALonelyPlace/files/modules/backPack.js
+++ ALonelyPlace/files/modules/backPack.js
@@ -37,7 +37,7 @@ export class backPack
                 console.log(itemsInInventory[i].innerHTML);
                 itemsInInventory[i].addEventListener("mouseenter", function(event)
                 {
-                    console.log(event.innerHTML);
+                    console.log(event); 
                 })
             }
             document.querySelector('.inventory-row').style.visibility = "visible";
