51392b93b6f33cf44200db5438f2f1bb01e8f03d
Montri <jg68332@gmail.com>
2022-01-23 10:58:48 +0100

    0.6.27

--- ALonelyPlace/files/modules/backPack.js
+++ ALonelyPlace/files/modules/backPack.js
@@ -34,9 +34,10 @@ export class backPack
             var itemsInInventory = document.querySelectorAll('.item');
             for(var i = 0; i < itemsInInventory.length; i++)
             {
+                console.log(itemsInInventory[i].innerHTML);
                 itemsInInventory[i].addEventListener("mouseenter", function(event)
                 {
-                    alert("HOVERED ITEM: " + itemsInInventory[i].innerHTML);
+                    alert("HOVERED ITEM");
                 })
             }
             document.querySelector('.inventory-row').style.visibility = "visible";
