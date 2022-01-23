f5e1486be0810cbebad71482a39abb10bf4ec6aa
Montri <jg68332@gmail.com>
2022-01-23 12:43:00 +0100

    0.6.44

--- ALonelyPlace/files/modules/php/getPlayerItems.php
+++ ALonelyPlace/files/modules/php/getPlayerItems.php
@@ -16,24 +16,27 @@
         $result = mysqli_query($conn, $sqlUser);
         $userInfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-        for($i = 0; $i < 5; $i++)
-        {
-            $itemSelected = 'item'.$i+1; 
-            $descriptionSelected = 'description'.$i+1;
-            $effectSelected = 'effect'.$i+1;
-            echo "ITEM SELECTED: " . $userInfo[0][$itemSelected];
-            $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0][$itemSelected]));
-            $item = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            if(count($item) < 0)
-            {
-                $userInfo[0][$descriptionSelected] = $item[0]['description'];
-                $userInfo[0][$effectSelected] = $item[0]['effect'];
-            }
-        }
+        $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0]['item1']));
+        $item1 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0]['item2']));
+        $item2 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0]['item3']));
+        $item3 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0]['item4']));
+        $item4 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        $result = mysqli_query($conn, "SELECT * FROM items WHERE id = " . intval($userInfo[0]['item5']));
+        $item5 = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
         echo $userInfo[0]['item1'] . ";" . $userInfo[0]['item2'] . ";" . $userInfo[0]['item3'] . ";" . $userInfo[0]['item4'] . ";" . $userInfo[0]['item5'] . ";";
         echo $userInfo[0]['rareity1'] . ";" . $userInfo[0]['rareity2'] . ";" . $userInfo[0]['rareity3'] . ";" . $userInfo[0]['rareity4'] . ";" . $userInfo[0]['rareity5'] . ";";
-        echo $userInfo[0]['description1'] . ";" . $userInfo[0]['description2'] . ";" . $userInfo[0]['description3'] . ";" . $userInfo[0]['description4'] . ";" . $userInfo[0]['description5'] . ";";
-        echo $userInfo[0]['effect1'] . ";" . $userInfo[0]['effect2'] . ";" . $userInfo[0]['effect3'] . ";" . $userInfo[0]['effect4'] . ";" . $userInfo[0]['effect5'];
+        echo $item1[0]['description'] . ";" . $item2[0]['description'] . ";" . $item3[0]['description'] . ";" . $item4[0]['description'] . ";" . $item5[0]['description'] . ";";
+        echo $item1[0]['effect'] . ";" . $item2[0]['effect'] . ";" . $item3[0]['effect'] . ";" . $item4[0]['effect'] . ";" . $item5[0]['effect'] . ";";
     }
     mysqli_close($conn);
 ?>
