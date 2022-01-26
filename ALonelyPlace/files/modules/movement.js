8d214c2a80b976ff4b55bf8a6b6a1554dc89a9f6
Justin <jg68332@gmail.com>
2022-01-26 10:56:38 +0100

    0.7.07

--- ALonelyPlace/files/modules/movement.js
+++ ALonelyPlace/files/modules/movement.js
@@ -19,27 +19,26 @@ export class Movement
         $(document).keydown(function() {
             var keyDown = event.keyCode;
             player.getPlayerObject().scrollIntoView();
-            collsion.friendlyPlayerCollsion(keyDown);
-            if(event.keyCode == 87 /*W*/ && event.keyCode != collsion.playerCollsion(keyDown))
+            if(event.keyCode == 87 /*W*/ && event.keyCode != collsion.playerCollsion(keyDown) && event.keyCode != collsion.friendlyPlayerCollsion(keyDown)) 
             {
                 position = player.getPlayerOffSetTop() - 10;
                 //PLAYER_OBJECT.style.top = position + "px";
                 player.addToTopPosition(position);
                 player.playPlayerWalkSound();
             }
-            else if(event.keyCode == 83 /*S*/&& event.keyCode != collsion.playerCollsion(keyDown))
+            else if(event.keyCode == 83 /*S*/&& event.keyCode != collsion.playerCollsion(keyDown)&& event.keyCode != collsion.friendlyPlayerCollsion(keyDown))
             {
                 position = player.getPlayerOffSetTop() - 6;
                 player.addToTopPosition(position);
                 player.playPlayerWalkSound();
             }
-            else if(event.keyCode == 68 /*D*/&& event.keyCode != collsion.playerCollsion(keyDown))
+            else if(event.keyCode == 68 /*D*/&& event.keyCode != collsion.playerCollsion(keyDown)&& event.keyCode != collsion.friendlyPlayerCollsion(keyDown))
             {
                 position = player.getPlayerOffSetLeft() - 6;
                 player.addToLeftPosition(position);
                 player.playPlayerWalkSound();
             }
-            else if(event.keyCode == 65 /*A*/&& event.keyCode != collsion.playerCollsion(keyDown))
+            else if(event.keyCode == 65 /*A*/&& event.keyCode != collsion.playerCollsion(keyDown)&& event.keyCode != collsion.friendlyPlayerCollsion(keyDown))
             {
                 position = player.getPlayerOffSetLeft() - 10;
                 player.addToLeftPosition(position);
