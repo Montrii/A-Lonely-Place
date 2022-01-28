b3f2542f9955c9972bf89863b8476ef085e21873
Montri <jg68332@gmail.com>
2022-01-29 00:38:55 +0100

    0.8.11

--- ALonelyPlace/files/modules/php/enterNextLevel.php
+++ ALonelyPlace/files/modules/php/enterNextLevel.php
@@ -17,6 +17,7 @@
         $result = mysqli_query($conn, $sqlUser);
         $userInfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
         $mapLevel = intval($userInfo[0]['map']); 
+        echo $mapLevel;
         $result = mysqli_query($conn, "UPDATE inventory SET map = " . $mapLevel+1 . " WHERE user = '" . $_GET['ip'] . "'");
     }
     mysqli_close($conn);
