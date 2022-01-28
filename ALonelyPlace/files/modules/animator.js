d58495cc7615326fa467315a1c3735f503909b0a
Montri <jg68332@gmail.com>
2022-01-28 20:02:02 +0100

    0.7.136

--- ALonelyPlace/files/modules/animator.js
+++ ALonelyPlace/files/modules/animator.js
@@ -3,16 +3,17 @@ import { Player } from "./player.js";
 export class Animator
 {
     playerAnimationInterval = 0;
-    player = new Player(); 
-    constructor(player)
+    constructor()
     {
 
     }
     playerPlayAnimation()
     {
         var id = 1;
+        var player = new Player(); 
         const interval = setInterval(function()
         {
+            
             player.getPlayerImage().src = "../../../Assets/monsters_idle/skeleton1/v2/skeleton_32_"+id+".png";
             if(id >= 4)
             {
