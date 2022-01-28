44fd42ed31a1cf216b3b0fab47284955f4495ac9
Montri <jg68332@gmail.com>
2022-01-28 23:24:25 +0100

    0.7.77

--- ALonelyPlace/files/modules/utilities.js
+++ ALonelyPlace/files/modules/utilities.js
@@ -196,17 +196,25 @@ export function defineRareityLevel()
     // RAREITY LEVELS
     // 4 -> legendary, 3 -> ultra, 2 -> epic, 1 -> rare, 0 -> common
     // legi -> 1%, ultra -> 3%, epic -> 8%, rare -> 30%, common -> 58% 
-    switch(random)
+    if(random == 1)
     {
-        case 1:
-            return 4;
-        case 2,3,4:
-            return 3;
-        case 5,6,7,8,9,10,11,12,13:
-            return 2;
-        case 14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43:
-            return 1;
-        case 44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100:
-            return 0;
+        rareity = 4;
     }
+    else if(random >= 2 && random <= 4)
+    {
+        rareity = 3;
+    }
+    else if(random >= 5 && random <= 13)
+    {
+        rareity = 2;
+    }
+    else if(random >= 14 && random <= 43)
+    {
+        rareity = 1;
+    }
+    else if(random <= 44 && random >= 100)
+    {
+        rareity = 0;
+    }
+    return rareity;
 }
