fb81ef58d61d22d1f68ee3494efd04da24f8b707
Montri <jg68332@gmail.com>
2022-01-28 23:58:00 +0100

    0.7.89

--- ALonelyPlace/files/modules/php/saveStats.php
+++ ALonelyPlace/files/modules/php/saveStats.php
@@ -16,60 +16,47 @@
         // VARIABLES & SQL STATEMENTS
         $ITEM_ID = intval($_GET['item']);
         $RAREITY = intval($_GET['rareity']);
-        $MATCH_ITEM = false;
-        $sql = "SELECT * FROM items WHERE id = " . $ITEM_ID;
         $sqlUser = "SELECT * FROM inventory WHERE user = '" . $_GET['ip'] . "'";
         $sqlInsert = "INSERT INTO inventory (user, item1, rareity1) VALUES('" . $_GET['ip'] . "'," . $ITEM_ID . ", " . $RAREITY . ")";
 
         // Get Item slot
-        $result = mysqli_query($conn, $sql);
-        $item = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        if(count($item) < 1)
+        $result = mysqli_query($conn, $sqlUser);
+        $userInfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        if($userInfo[0]['item1'] == 0 || $userInfo[0]['rareity1'] == 0)
         {
-            echo "THIS ITEM DOES NOT EXIST IN OUR DATABASE!";
-        }
-        else 
-        {
-            echo "ITEM FOUND IN DATABASE!\n";
-
-            $result = mysqli_query($conn, $sqlUser);
-            $userInfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            if(count($userInfo) < 1)
-            {
                 // insert new data into database
-                echo "USER DATA DOES NOT EXIST\n";
-                $result = mysqli_query($conn, $sqlInsert);
-                echo "SUCCESSFULLY WRITTEN DATA INTO DATABASE!\n";
-            }
-            else
+            echo "USER DATA DOES NOT EXIST\n";
+            $result = mysqli_query($conn, $sqlInsert);
+            echo "SUCCESSFULLY WRITTEN DATA INTO DATABASE!\n";
+        }
+        else
+        {
+            // update inventory
+            echo "USER DATA EXISTS - OVERWRITING\n";
+            $item2 = intval($userInfo[0]['item2']);
+            $item3 = intval($userInfo[0]['item3']);
+            $item4 = intval($userInfo[0]['item4']);
+            if($item2 == 0)
             {
-                // update inventory
-                echo "USER DATA EXISTS - OVERWRITING\n";
-                $item2 = intval($userInfo[0]['item2']);
-                $item3 = intval($userInfo[0]['item3']);
-                $item4 = intval($userInfo[0]['item4']);
-                if($item2 == 0)
-                {
-                    $result = mysqli_query($conn, "UPDATE inventory SET item2 = " . $ITEM_ID . "rareity2 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
-                    echo $result;
-                }
-                else if($item3 == 0)
-                {
-                    $result = mysqli_query($conn, "UPDATE inventory SET item3 = " . $ITEM_ID . "rareity3 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
-                    echo $result;
-                }
-                else if($item4 == 0)
-                {
-                    $result = mysqli_query($conn, "UPDATE inventory SET item4 = " . $ITEM_ID . "rareity4 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
-                    echo $result;
-                }
-                else 
-                {
-                    $result = mysqli_query($conn, "UPDATE inventory SET item5 = " . $ITEM_ID . "rareity5 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
-                    echo $result;
-                }
-                echo "SUCCESSFULLY UPDATED USER DATA IN DATABASE!";
+                $result = mysqli_query($conn, "UPDATE inventory SET item2 = " . $ITEM_ID . "rareity2 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
+                echo $result;
             }
+            else if($item3 == 0)
+            {
+                $result = mysqli_query($conn, "UPDATE inventory SET item3 = " . $ITEM_ID . "rareity3 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
+                echo $result;
+            }
+            else if($item4 == 0)
+            {
+                $result = mysqli_query($conn, "UPDATE inventory SET item4 = " . $ITEM_ID . "rareity4 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
+                echo $result;
+            }
+            else 
+            {
+                $result = mysqli_query($conn, "UPDATE inventory SET item5 = " . $ITEM_ID . "rareity5 = " . $RAREITY . " WHERE user = '" . $_GET['ip'] . "'");
+                echo $result;
+            }
+            echo "SUCCESSFULLY UPDATED USER DATA IN DATABASE!";
         }
         mysqli_close($conn);
     }
