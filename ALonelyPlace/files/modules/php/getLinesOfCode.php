c93f293506ef49084a0dba8ccb7431cc259dff4d
Montri <jg68332@gmail.com>
2022-01-29 13:19:38 +0100

    0.8.6

--- ALonelyPlace/files/modules/php/getLinesOfCode.php
+++ ALonelyPlace/files/modules/php/getLinesOfCode.php
@@ -2,7 +2,10 @@
 $path = realpath('../../../');
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $filename)
 {
+    if(is_dir($filename) == false)
+    {
         echo "$filename <br/>";
+    }
 }
 
 
