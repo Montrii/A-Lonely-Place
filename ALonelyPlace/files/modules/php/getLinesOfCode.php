95337cd2d4195e23d112c144efc0c51e31a45ce1
Montri <jg68332@gmail.com>
2022-01-29 13:21:33 +0100

    0.8.61

--- ALonelyPlace/files/modules/php/getLinesOfCode.php
+++ ALonelyPlace/files/modules/php/getLinesOfCode.php
@@ -1,10 +1,19 @@
 <?php
 $path = realpath('../../../');
+$linecount = 0;
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $filename)
 {
     if(is_dir($filename) == false)
     {
         echo "$filename <br/>";
+        $handle = fopen($filename, "r");
+        while(!feof($handle))
+        {
+            $line = fgets($handle, 4096);
+            $linecount = $linecount + substr_count($line, PHP_EOL);
+        }
+
+        fclose($handle);
     }
 }
 
